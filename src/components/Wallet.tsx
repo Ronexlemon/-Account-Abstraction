1e83c2c631bcf751122ada7c4fe0cffcce53ee2d
RonexLemon <ronexondimu@gmail.com>
2024-02-24 08:45:53 +0300

    bundler

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -3,11 +3,15 @@
 import { useEffect, useRef, useState } from "react";
 import SocialLogin from "@biconomy/web3-auth";
 import { ChainId } from "@biconomy/core-types";
-import { ethers } from "ethers";
+import { ethers, providers } from "ethers";
+import { BiconomySmartAccountV2, BiconomySmartAccountV2Config } from "@biconomy/account";
 
 export default function Wallet() {
   const sdkRef = useRef<SocialLogin | null>(null);
   const [interval, enableInterval] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
+const [, setProvider] = useState<providers.Web3Provider>();
+const [smartAccount, setSmartAccount] = useState<BiconomySmartAccountV2>();
 
   // login() function
   async function login() {
@@ -31,6 +35,39 @@ export default function Wallet() {
     }
   }
   // setupSmartAccount() function
+  async function setupSmartAccount() {
+    try {
+      // If the SDK hasn't fully initialized, return early
+      if (!sdkRef.current?.provider) return;
+  
+      // Hide the wallet if currently open
+      sdkRef.current.hideWallet();
+  
+      // Start the loading indicator
+      setLoading(true);
+  
+      // Initialize the smart account
+      let web3Provider = new ethers.providers.Web3Provider(
+        sdkRef.current?.provider
+      );
+      setProvider(web3Provider);
+      const config: BiconomySmartAccountV2Config = {
+        signer: web3Provider.getSigner(),
+        chainId: ChainId.POLYGON_MUMBAI,
+        bundler: bundler,
+        paymaster: paymaster,
+      };
+      const smartAccount = new BiconomySmartAccountV2(config);
+      await smartAccount.init();
+  
+      // Save the smart account to a state variable
+      setSmartAccount(smartAccount);
+    } catch (e) {
+      console.error(e);
+    }
+  
+    setLoading(false);
+  }
 
   useEffect(() => {
     let configureLogin: NodeJS.Timeout | undefined;
